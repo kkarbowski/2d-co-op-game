1f6b92b01da7e22dadd8307984e258b50678e896
Mateusz <wszeborowskimateusz@gmail.com>
2018-05-17 16:15:23 +0200

    Get rid of nasty things

--- src/coopgame/Coopgame.java
+++ src/coopgame/Coopgame.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package coopgame;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -12,10 +7,7 @@ import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
 
-/**
- *
- * @author Korneliusz
- */
+
 public class Coopgame extends BasicGame
 {
 	public Coopgame(String gamename)
